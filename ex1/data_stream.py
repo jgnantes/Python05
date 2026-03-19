3a79c036e59c8967ef9d48652dde753d86ba2df3
jgnantes <jgnantes@outlook.com>
2026-03-19 00:39:20 -0300

    aporra

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -5,10 +5,9 @@ from abc import ABC, abstractmethod
 class DataStream(ABC):
     """ """
 
-    @abstractmethod
-    def process_batch(self, data_batch: List[Any]) -> str:
-        """ """
-        pass
+    def init(self, stream_id: int) -> None:
+        self.stream_id: int = stream_id
+        self.stats: Dict[str, Union[str, int, float]] = {}
 
     def filter_data(
         self, 
@@ -18,8 +17,15 @@ class DataStream(ABC):
         """ """
         ...
 
+    @abstractmethod
+    def process_batch(self, data_batch: List[Any]) -> str:
+        """ """
+        pass
+
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
         """ """
-        ...
+        return self.stats
 
 
+if __name__ == "__main__":
+    
